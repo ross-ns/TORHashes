680e86cc59e5e3f30ad4dc6dbc417d5614531b44
Ross <23499787+ross-ns@users.noreply.github.com>
2025-05-28 09:51:34 +0100

    Initial commit

---
+++ Get-TorBrowserHashes.ps1
@@ -0,0 +1,26 @@
+$knownVersions = Get-Content ./knownVersions.txt        #Known versions of the TOR Browser bundle. Stops updates if the version is already known. Must contain at least one version
+$baseUrl = "https://dist.torproject.org/torbrowser/"
+$signedHashFile = "sha256sums-signed-build.txt"         #Filename containing the hashes of the signed builds of the TOR Browser
+$unsignedHashFile = "sha256sums-unsigned-build.txt"     #Filename containing the hashes of the unsigned builds of the TOR Browser
+$hashes = @()
+
+#Scrape the TOR download page for links
+$links = (Invoke-WebRequest -Uri $baseUrl).Links.Href | Get-Unique
+
+foreach ($link in $links) {
+    $parts = $link.Split(".")
+
+    if ($parts[0] -match "^\d+$") {
+        if (!$knownVersions.Contains($link)) {
+            $link | Out-File ./knownVersions.txt -Encoding ascii -Append -Force
+
+            Invoke-WebRequest -Uri (-join ($baseUrl, $link, $signedHashFile)) -OutFile (-join ($link.Trim("/"), "_", $signedHashFile))
+            Invoke-WebRequest -Uri (-join ($baseUrl, $link, $unsignedHashFile)) -OutFile (-join ($link.Trim("/"), "_", $unsignedHashFile))
+
+            $hashes += Import-Csv -Delimiter " " -Header "Hashes" -Path (-join ("./", $link.Trim("/"), "_", $signedHashFile))
+            $hashes += Import-Csv -Delimiter " " -Header "Hashes" -Path (-join ("./", $link.Trim("/"), "_", $unsignedHashFile))
+        }
+    }
+}
+
+$hashes | Out-File ./tor_browser_hashes.txt -Encoding ascii -Append -Force
